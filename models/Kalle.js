dd9a614605760ec6daf18cfe2f48ff357a28206c
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-10-06 14:18:19 +0300

    Implemented Individual Schema for each profile

---
+++ models/Kalle.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    messageContent: {
+        type: String,
+        required: true,
+        maxLength: 25,
+    },
+    timeOfMessage: {
+        type: String,
+    },
+});
+
+module.exports = mongoose.model('Kalle', messageSchema);
